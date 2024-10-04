b64d44fc5451dfc764be60c5c8554b1dff060b0d
Rohan <159823468+Rohankhan999@users.noreply.github.com>
2024-10-04 11:40:04 +0500

    .

---
+++ app.js
@@ -0,0 +1,15 @@
+//Head and Tail//
+
+// var player1 = prompt("Enter player 1 name!");
+// var player2 = prompt("Enter player 2 name");
+// var number = +prompt("Player no 1 Enter your number (0 or 1)");
+
+// const randomNumber = Math.floor(Math.random() * 2)
+// console.log(randomNumber);
+
+// if (randomNumber === number) {
+//     document.write(player1 + " You Win! " + " " + player2 + " Better luck next time  ");
+// } 
+// else {
+//     document.write(player2 +  "You Win" + " " + player1 + " Better luck next time  ");
+// }
